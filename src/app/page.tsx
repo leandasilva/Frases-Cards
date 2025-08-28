10191bf28e704ea3f540cfb122310208b1def78f
Leandro <leandro_delmillo@hotmail.com>
2025-08-28 20:18:44 -0300

    se agrego navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,11 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "@/store/store";
-import { fetchFrases } from "@/features/frases/frasesSlice"; 
+import { fetchFrases } from "@/features/frases/frasesSlice";
 import SearchBar from "@/features/frases/component/SearchBar";
 import FraseList from "@/features/frases/component/FraseList";
 import { useRouter } from "next/navigation";
 import "../styles/globals.css";
-
 
 export default function AddCardPage() {
   const dispatch = useDispatch<AppDispatch>();
@@ -18,25 +17,34 @@ export default function AddCardPage() {
     dispatch(fetchFrases());
   }, [dispatch]);
 
-   const ViewAgregarFrase = () => {
+  const ViewAgregarFrase = () => {
     router.push("/frases/addFrase");
   };
 
   return (
-  <div className="min-h-screen bg-gradient-to-br from-[#1a1a2e] via-[#533483] to-[#0f3460] bg-gradient-to-bl from-[#1a1a2e] via-[#533483] to-[#0f3460] p-6">    
-      <h1 className="text-center text-white text-3xl font-bold mb-8">
-        Listado de Frases
-      </h1>
+    <div className="min-h-screen bg-gradient-to-br from-[#1a1a2e] via-[#533483] to-[#0f3460]">
+      {/* Navbar */}
+      <nav className="bg-[#0f172a] shadow-md py-4 px-6">
+        <h1 className="text-white text-xl font-bold">📘 Proyecto de Leandro</h1>
+      </nav>
 
-      <div className="flex justify-between mb-6 gap-4">
-        <button 
-        onClick={ViewAgregarFrase}
-        className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg shadow">
-          Agregar Frase
-        </button>      
+      <div className="p-6">
+        <h1 className="text-center text-white text-3xl font-bold mb-8">
+          Listado de Frases
+        </h1>
+
+        <div className="flex justify-between mb-6 gap-4">
+          <button
+            onClick={ViewAgregarFrase}
+            className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg shadow"
+          >
+            Agregar Frase
+          </button>
+        </div>
+
+        <SearchBar />
+        <FraseList />
       </div>
-      <SearchBar />
-      <FraseList />
     </div>
   );
 }
